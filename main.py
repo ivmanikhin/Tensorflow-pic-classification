60003c5d18120cb53719c6a630379f1161a93e3e
ivmanikhin <ivmanikhin@gmail.com>
2022-03-09 12:36:12 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+from sklearn.datasets import make_circles
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Make synthetic dataset
+n_samples = 1000
+X, y = make_circles(n_samples, noise=0.03, random_state=12)
+
+# Visualize data
+circles = pd.DataFrame({"X0": X[:, 0], "X1": X[:, 1], "label": y})
+print(circles)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.RdYlBu)
+plt.show()
