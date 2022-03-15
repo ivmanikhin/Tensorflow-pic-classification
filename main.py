da0c7b5f81fed0e6f26124aa94e3eb64c4ca6bf1
ivmanikhin <ivmanikhin@gmail.com>
2022-03-15 18:29:41 +0300

    made some visualisation

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ import tensorflow as tf
 import sklearn
 from sklearn.datasets import make_circles
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
+
+def plot_decision_boundary(model, X, y):
+    x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
+    y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100), np.linspace(y_min, y_max, 100))
+    x_in = np.c_[xx.ravel(), yy.ravel()] #stack 2D arrays together
+    y_pred = model.predict(x_in)
+    if len(y_pred[0]) > 1:
+        y_pred = np.argmax(y_pred, axis=1).reshape(xx.shape)
+    else:
+        y_pred = np.round(y_pred).reshape(xx.shape)
+    plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.show()
 
 # Make synthetic dataset
 n_samples = 1000
@@ -22,7 +39,10 @@ print(y[0])
 # make train and test datasets
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
 
+
+
 # Modelling
+tf.random.set_seed(32)
 leaky_relu = tf.keras.layers.LeakyReLU(alpha=0.1)
 model = tf.keras.Sequential([
     tf.keras.Input(shape=(2,)),
@@ -34,6 +54,8 @@ model.compile(loss=tf.keras.losses.BinaryCrossentropy(),
               optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
               metrics=["accuracy"])
 
-model.fit(X_train, y_train, epochs=100)
+model.fit(X_train, y_train, epochs=50)
 
 model.evaluate(X_test, y_test)
+
+plot_decision_boundary(model=model, X=X, y=y)
